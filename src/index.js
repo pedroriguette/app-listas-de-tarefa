86db6b130aee0b936937885b77cb8198b4974495
pedroriguette <pedromedeiros062005@gmail.com>
2026-07-31 17:07:12 -0300

    index settings completed

--- src/index.js
+++ src/index.js
@@ -7,4 +7,17 @@ const app = express()
 
 // Configurações do EJS
 app.set('view engine', 'ejs')
-app.set()
+app.set('views', path.join(__dirname, 'views'))
+
+// Configuração de arquivos estaticos
+app.use(express.static('public'))
+
+// Configuração para ler dados da requisição
+app.use(express.urlencoded({ extended: true }))
+
+// Rotas da aplicação 
+app.use(router)
+
+const PORT = 3000
+
+app.listen(PORT, () => console.log(`Iniciando em: http://localhost:${PORT}`))
