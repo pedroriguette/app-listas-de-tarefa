c7611b749db90bbc45879bc9a549afa338914a1d
pedroriguette <pedromedeiros062005@gmail.com>
2026-08-17 17:11:12 -0300

    taskController organized

--- src/controllers/tasksController.js
+++ src/controllers/tasksController.js
@@ -2,14 +2,14 @@ const listOfTaskModel = require('../models/taskModel')
 
 
 const taskController = {
-    // All tasks
+    // All ListTasks
     index: (req, res) => {
         const tasks = listOfTaskModel.getAllTasks()
 
         res.render('index', { tasks })
     },
 
-    // Get task
+    // Get ListTask
     show: (req, res) => {
         const id = req.params.id
         const task = listOfTaskModel.getListById(id)
@@ -17,21 +17,50 @@ const taskController = {
         res.render('task', { task })
     },
 
-    // Get task form
+    // Get ListTask form
     create: (req, res) => {
         res.render('newTaskForm')
     },
 
-    // Post task
-    postTask: (req, res) => {
+    // Post ListTask
+    postListTask: (req, res) => {
         const { name, tasks } = req.body
-        const tasksArray = Array.isArray(tasks) ? tasks : [tasks]
-        const list = listOfTaskModel.postTask(
-            name, tasksArray
+        const arrayTask = []
+        if (Array.isArray(tasks)) {
+            tasks.forEach(t => {
+                arrayTask.push(
+                    {
+                        id_task: Math.floor(Math.random() * 99999).toString(),
+                        task: t,
+                        check: false
+                    }
+                )
+            })
+        } else {
+            arrayTask.push(
+                {
+                    id_task: Math.floor(Math.random() * 99999).toString(),
+                    task: tasks,
+                    check: false
+                }
+            )
+        }
+
+        const list = listOfTaskModel.postListTask(
+            name, arrayTask
         )
 
         listOfTaskModel.save(list)
         res.redirect('/tasks')
+    },
+
+    // delete ListTask
+    deleteTask: (req, res) => {
+        const id = req.params.id
+
+        listOfTaskModel.deleteList(id)
+
+        res.redirect("/tasks")
     },
 
     // add task in array task
@@ -41,16 +70,28 @@ const taskController = {
         const id = req.params.id
         const task = listOfTaskModel.getListById(id)
 
-        task.tasks.push(tasks)
+        task.tasks.push(
+            {
+                id_task: Math.floor(Math.random() * 99999).toString(),
+                task: tasks,
+                check: false
+            }
+        )
         res.redirect(`/task/${id}`)
     },
 
-    deleteTask: (req, res) => {
+    // Completed task
+    completedTask: (req, res) => {
+        const { check } = req.body
         const id = req.params.id
+        const id_task = req.params.id_task
 
-        listOfTaskModel.deleteList(id)
+        const listTask = listOfTaskModel.getListById(id)
+        listTask.tasks.find(t => t.id_task == id_task)
+        .check = check === 'true' ? true : false
 
-        res.redirect("/tasks")
+        listOfTaskModel.updateList(id, listTask)
+        res.redirect(`/task/${id}`)
     }
 }
 
